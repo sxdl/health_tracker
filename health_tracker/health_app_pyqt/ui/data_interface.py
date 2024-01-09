4d6f993cdc17f51f21fdbb201ecbd884d09b8b04
sxdl <3265168281@qq.com>
2024-01-09 23:14:54 +0800

    my data: 补充上了剩余数据的图表html

--- health_tracker/health_app_pyqt/ui/data_interface.py
+++ health_tracker/health_app_pyqt/ui/data_interface.py
@@ -40,9 +40,29 @@ class DataInterface(QWidget, Ui_DataInterface):
             需要写JS或用其他接口方法把本地数据读入HTML进行渲染，
             且后续要仿照该示例添加其他图表
             """
+            # Calories
+            # TODO: 添加“累计”和“日均”label
             self.webview = QWebEngineView()
-            self.webview.load(self.renderLocalHtmlURL("area-stack-gradient.html"))
-            self.verticalLayout.addWidget(self.webview)
+            self.webview.load(self.renderLocalHtmlURL("calories-daily-area-stack-gradient.html"))
+            self.verticalLayout_1_1.addWidget(self.webview)
+
+            # Floor
+            # TODO: 爬楼数据应该要改成柱状图，展示每日的数据；添加“总高度”和“平均高度”label
+            self.webview = QWebEngineView()
+            self.webview.load(self.renderLocalHtmlURL("floor-daily-area-stack-gradient.html"))
+            self.verticalLayout_2_1.addWidget(self.webview)
+
+            # Distance
+            # TODO: 添加“总距离”和“平均距离”label
+            self.webview = QWebEngineView()
+            self.webview.load(self.renderLocalHtmlURL("distance-daily-area-stack-gradient.html"))
+            self.verticalLayout_3_1.addWidget(self.webview)
+
+            # Steps
+            # TODO: 步数数据应该要改成柱状图，展示每日的数据；添加“总步数”和“平均步数”label
+            self.webview = QWebEngineView()
+            self.webview.load(self.renderLocalHtmlURL("steps-daily-area-stack-gradient.html"))
+            self.verticalLayout_4_1.addWidget(self.webview)
 
             # 添加菜单，用于切换图表
             self.pivot.addItem(
