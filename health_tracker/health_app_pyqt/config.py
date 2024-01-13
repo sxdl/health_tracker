734dc79013156c743b516d626a83f7b11c719d75
Yinan Zhu <202264691080@mail.scut.edu.cn>
2024-01-13 20:22:55 +0800

    add: 新增设置页面

--- health_tracker/health_app_pyqt/config.py
+++ health_tracker/health_app_pyqt/config.py
@@ -2,3 +2,4 @@ import os
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 GITHUB = "https://github.com/sxdl/health_tracker"
+FEEDBACK_URL = "https://github.com/sxdl/health_tracker/issues"
